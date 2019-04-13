7b322915e4aa8b1213a3186beac9d8ac8cf03b8f
wfy815775 <815775754@qq.com>
2019-04-13 10:03:18 +0800

    226. 翻转二叉树

---
+++ src/datastructure/DSOne.go
@@ -0,0 +1,19 @@
+package datastructure
+
+//Definition for a binary tree node.
+type TreeNode struct {
+	Val         int
+	Left, Right *TreeNode
+}
+
+//226. 翻转二叉树
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	leftNode, rightNode := root.Left, root.Right
+	invertTree(leftNode)
+	invertTree(rightNode)
+	root.Right, root.Left = leftNode, rightNode
+	return root
+}
